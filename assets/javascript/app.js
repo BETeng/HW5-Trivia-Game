120ed17d8d7b554ccd8f1b5a983587e66dd7cda8
BETeng <brian7.teng@gmail.com>
2019-04-15 16:39:39 -0500

    more logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ var countdown = 0;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-var secondsCounter = 3;
+var secondsCounter = 10;
 var intervalId='';
 var questions = [{
   prompt:'what color be apples?',
@@ -53,53 +53,82 @@ function StartInterval(){
     console.log(secondsCounter);
     if (secondsCounter <= 0) {
       clearInterval(intervalId);
-      unanswered++;
       showAnswer();
       runningQuestion++;
+      // setTimeout(checkAnswer(), 5000);
     }
+    else if($('.choice').click()){
+      stopTimer();
+    }
+      
     else{
       $('#htmlTimer').text('Time Remaining: '+secondsCounter);
     }
   }, 1000)
 }
-$('.choice').click(function(){
-  console.log('this: '+this.id);
-  
-  userAnswer=this.id;
-  console.log('useranswer: '+userAnswer);
-})
+// $('.choice').click(function(){
+//   console.log('this: '+this.id);
+//   userAnswer=this.id;
+//   console.log('useranswer: '+userAnswer);
+//   checkAnswer();
+// })
+
 function renderQuestion(){
   let q = questions[runningQuestion];
   $('#question').text(q.prompt);
   $('#A').text(q.choiceA);
   $('#B').text(q.choiceB);
   $('#C').text(q.choiceC);
-  
-  
+  console.log("right answer: "+q.answer)
 }
-function showAnswer(){
+
+function showAnswer(){// set timer for this part of function
   $('#question').empty();
   $('.choice').empty();
-  
   console.log("showAnswer works");
-
+  clearInterval(intervalId);
 }
 
+userAnswer = $(this).click
+  
+function stopTimer(){
+  $('.choice').on('click', function(){
+    clearInterval(intervalId);
+  })
+}
 
-
+// checks for:
+  //if user click = answer
+  //puts a mesage on board
+  //increment the score counter
+  //check to see if any questions remaining
+  //if no more questions then hides the quiz box and displays the score
 function checkAnswer(userAnswer){
   if(questions[runningQuestion].answer == userAnswer){
+    $('#question').empty();
+    console.log('useranswer ' +userAnswer)
+    $('#question').text("You got the correct answer!")
+    console.log("u clicked right asnwer")
     correct++;
   }
   else{
+    console.log('useranswer ' +userAnswer)
+    $('#question').empty();
+    $('#question').text("Sorry, the correct answer is: "+questions[runningQuestion].answer)
     incorrect++;
+    console.log('u click wrong answer')
   }
-  if(runningQuestion<lastQuestion){
+  if(runningQuestion<lastQuestion){ 
     runningQuestion++;
     renderQuestion();
   }
   else{
-    console.log('thats it')
+    $('#quiz').hide();
+    $('#numCorrect').text('Correct: '+correct);
+    $('#numIncorrect').text('Incorrect: '+incorrect);
+    $('#numUnanswered').text('Unanswered: '+unanswered);
+    $('#score-board').show();
+    console.log('End of the questions');
   }
 }
 
